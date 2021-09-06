29e9f6925427aad4d964dc7c8c9305a0126953e6
Apinanz <apinantakun_n@silpakorn.edu>
2021-09-06 21:53:32 +0700

    แก้ล่าสุด server & client

--- src/Client.java
+++ src/Client.java
@@ -1,34 +1,36 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
-import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 
 public class Client {
-    public static final int PORT = 8087;
     Socket clientSocket;
     DataInputStream din;
     DataOutputStream dout;
+    String[][] fileName;
 
     public static void main(String[] args) {
-        new Client();
-        JFrame frameLog = new JFrame();
-        frameLog.setTitle("Client");
-        frameLog.setSize(500, 800);
-        frameLog.setVisible(true);
+        new Client().Model();
 
     }
 
     public Client() {
         // connect to server
+        int file;
 
         try {
-
             clientSocket = new Socket("localhost", 8087);
             din = new DataInputStream(clientSocket.getInputStream());
             dout = new DataOutputStream(clientSocket.getOutputStream());
 
-            listenForInput();
+            file = din.readInt();
+            fileName = new String[file][2];
+            for (int i = 0; i < file; i++) {
+                fileName[i][0] = din.readUTF();
+            }
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Can't Connected. Please Try Again!", "ERROR CONNECTED",
@@ -37,9 +39,20 @@ public class Client {
         }
     }
 
-    public void listenForInput() {
-        while (true) {
+    public void Model() {
+        JFrame frameFile = new JFrame();
+        frameFile.setTitle("Client");
+        frameFile.setSize(500, 800);
+        frameFile.setVisible(true);
 
-        }
+        String[] col = { "File", "Download" };
+        JPanel jpFile = new JPanel();
+        JTable jtFile = new JTable(fileName, col);
+        JScrollPane jspFile = new JScrollPane(jtFile);
+        jpFile.setBounds(0, 0, 500, 400);
+        jpFile.add(jspFile);
+        frameFile.getContentPane().add(jpFile);
+
+        
     }
 }
