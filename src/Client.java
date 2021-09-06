42e4b23edf85972aa2e26353bca840d55e079fdd
Apinanz <apinantakun_n@silpakorn.edu>
2021-09-06 17:37:33 +0700

    แก้ให้มี GUI หน้าหลัก

--- src/Client.java
+++ src/Client.java
@@ -3,24 +3,23 @@ import java.net.Socket;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-
-
 public class Client {
     public static final int PORT = 8087;
+
     public static void main(String[] args) {
 
-
-        //connect to server
+        JFrame frameLog = new JFrame();
+        frameLog.setTitle("Client");
+        frameLog.setSize(500, 800);
+        frameLog.setVisible(true);
+        // connect to server
         try {
-            JFrame frameLog = new JFrame();
-            frameLog.setTitle("Client");
-            frameLog.setSize(500, 800);
-            frameLog.setVisible(true); 
-            
-            Socket clientSocket = new Socket("localhost",8087);
+
+            Socket clientSocket = new Socket("localhost", 8087);
 
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Can't Connected. Please Try Again!","ERROR CONNECTED",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Can't Connected. Please Try Again!", "ERROR CONNECTED",
+                    JOptionPane.ERROR_MESSAGE);
 
         }
 
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,11 +9,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.BorderLayout;
 
 public class Server {
     public static final int PORT = 8087;
@@ -41,45 +46,106 @@ public class Server {
             arr[index++][0] = object;
         }
 
-        // Frame Server
-        JFrame frameServer = new JFrame();
-        frameServer.setSize(500, 800);
-        frameServer.setTitle("Server");
-        frameServer.setLayout(new BoxLayout(frameServer.getContentPane(), BoxLayout.Y_AXIS));
-        frameServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameServer.setVisible(true);
+        // frame Main Server
+        JFrame frameMain = new JFrame();
+        frameMain.setSize(500, 800);
+        frameMain.setTitle("Welcome to Server");
+        frameMain.setVisible(true);
 
-        // ตารางชื่อไฟล์และขนาด
-        JPanel jpFile = new JPanel();
-        JTable jtFile = new JTable(arr, col);
-        JScrollPane jspFile = new JScrollPane(jtFile);
-        jpFile.setBounds(0, 0, 500, 400);
-        jpFile.add(jspFile);
-        frameServer.getContentPane().add(jpFile);
+        JPanel jpMain = new JPanel();
+        jpMain.setLayout(new CardLayout());
+
+        // ส่วนของหน้าmain ปุ่ม File
+        JPanel jpBut = new JPanel();
+        JButton butFile = new JButton("File");
+        butFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Frame Server
+                JFrame frameServer = new JFrame();
+                frameServer.setSize(500, 800);
+                frameServer.setTitle("Server");
+                frameServer.setLayout(new BoxLayout(frameServer.getContentPane(), BoxLayout.Y_AXIS));
+                frameServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frameServer.setVisible(true);
+                frameMain.setVisible(false);
+
+                // ตารางชื่อไฟล์และขนาด
+                JPanel jpFile = new JPanel();
+                JTable jtFile = new JTable(arr, col);
+                JScrollPane jspFile = new JScrollPane(jtFile);
+                jpFile.setBounds(0, 0, 500, 400);
+                jpFile.add(jspFile);
+                frameServer.getContentPane().add(jpFile);
+
+                JPanel jpBack = new JPanel();
+                JButton butBack = new JButton("Back");
+                butBack.addActionListener(new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        frameServer.setVisible(false);
+                        frameMain.setVisible(true);
+                    }
+
+                });
+                jpBack.add(butBack);
+                frameServer.add(jpBack);
+            }
+
+        });
+        jpBut.add(butFile);
+
+        // ส่วนของหน้าmain ปุ่ม log
+        JButton butLog = new JButton("Log");
+        jpBut.add(butLog);
+        butLog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Jframe
+                JFrame frameLog = new JFrame();
+                frameLog.setLayout(new BoxLayout(frameLog.getContentPane(), BoxLayout.Y_AXIS));
+                frameLog.setTitle("Log");
+                frameLog.setSize(500, 800);
+
+                // Panel and Lable
+                JPanel jpLog = new JPanel();
+                JLabel jlLog = new JLabel();
+                jlLog.setText("Waiting to connecting from Client " + client.PORT);
+
+                jlLog.setText("Connecting from Client " + client.PORT);
+                jpLog.add(jlLog);
+                System.out.println("connect");
+
+                frameLog.add(jpLog);
+                frameLog.setVisible(true);
+                frameMain.setVisible(false);
+
+                JPanel jpBack = new JPanel();
+                JButton butBack = new JButton("Back");
+                butBack.addActionListener(new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        frameLog.setVisible(false);
+                        frameMain.setVisible(true);
+                    }
+
+                });
+                jpBack.add(butBack);
+                frameLog.add(jpBack);
+
+            }
+
+        });
+
+        jpMain.add(jpBut, "Panel 2");
+        frameMain.getContentPane().add(jpMain, BorderLayout.CENTER);
 
         // ส่วนของ connecting
 
         ServerSocket serverSocket = new ServerSocket(8087);
         try {
-            // Jframe
-            JFrame frameLog = new JFrame();
-            frameLog.setTitle("Log");
-            frameLog.setSize(500, 800);
-
-            // Panel and Lable
-            JPanel jpLog = new JPanel();
-            JLabel jlLog = new JLabel();
-            jlLog.setText("Waiting to connecting from Client " + client.PORT);
-
-           
-
-            jlLog.setText("Connecting from Client " + client.PORT);
-            jpLog.add(jlLog);
-            System.out.println("connect");
-
-            frameLog.add(jpLog);
-            frameLog.setVisible(true); 
-            
             Socket clientSocket = serverSocket.accept();
 
         } catch (Exception e) {
