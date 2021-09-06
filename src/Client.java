1bc695a2a0db4d093355854b17a0bda23df9a1ae
Apinanz <apinantakun_n@silpakorn.edu>
2021-09-06 14:31:44 +0700

    มีไฟล์ txt

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,24 @@
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
 
 public class Client {
+    public static final int port = 8080;
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+
+        //connect to server
+        try {
+            Socket socketCilen = new Socket("locathost", port);
+            
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Can't Connected. Please Try Again!","ERROR CONNECTED",JOptionPane.ERROR_MESSAGE);
+
+        }
+
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -13,12 +15,15 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+
 public class Server {
+    public static final int PORT = 8080;
 
     public static void main(String[] args) {
+        Client client = new Client();
 
-        //Reader name file
-        String path = "C:\\Users\\api_q\\OneDrive\\เดสก์ท็อป\\file.txt";
+        // Reader name file
+        String path = "C:\\Users\\api_q\\OneDrive\\เดสก์ท็อป\\OperatingSysProj\\OperatingSysProj\\file.txt";
         File file = new File(path);
 
         ArrayList<String> arrFile = new ArrayList<String>();
@@ -34,33 +39,47 @@ public class Server {
         }
         int index = 0;
         String[][] arr = new String[arrFile.size()][2];
-        String[] col = {"ไฟล์ทั้งหมด","ขนาด"};
+        String[] col = { "ไฟล์ทั้งหมด", "ขนาด" };
         for (String object : arrFile) {
             arr[index++][0] = object;
         }
 
-        //Frame Server
+        // Frame Server
         JFrame frameServer = new JFrame();
         frameServer.setSize(500, 800);
         frameServer.setTitle("Server");
         frameServer.setLayout(new BoxLayout(frameServer.getContentPane(), BoxLayout.Y_AXIS));
         frameServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameServer.setVisible(true);
-        
-        //menuBar
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menuFile = new JMenu("File");
-        JMenu menuLog = new JMenu("Log");
-        menuBar.add(menuFile);
-        menuBar.add(menuLog);
-        frameServer.setJMenuBar(menuBar);
-        
-        //ตารางชื่อไฟล์และขนาด
+
+        // ตารางชื่อไฟล์และขนาด
         JPanel jpFile = new JPanel();
-        JTable jtFile = new JTable(arr,col);
+        JTable jtFile = new JTable(arr, col);
         JScrollPane jspFile = new JScrollPane(jtFile);
-        jpFile.setBounds(0,0,500,400);
+        jpFile.setBounds(0, 0, 500, 400);
         jpFile.add(jspFile);
         frameServer.getContentPane().add(jpFile);
+
+        // ส่วนของ connecting
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            
+
+            //Jframe
+            JFrame frameLog = new JFrame();
+            frameLog.setTitle("Log");
+            frameLog.setSize(500, 800);
+
+            //Panel and Lable
+            JPanel jpLog = new JPanel();
+            JLabel jlLog = new JLabel();
+            jlLog.setText("Waiting to connecting from Client "+client.port);
+            Socket socket = serverSocket.accept();
+            jlLog.setText("Connecting from Client "+client.port);
+            jpLog.add(jlLog);
+        } catch (Exception e) {
+            System.out.println("0");
+        }
+
     }
 }
