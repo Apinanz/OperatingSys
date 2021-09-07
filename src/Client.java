7724a52d53b713e7973450c694387f9fd841e77f
Anupap Tubtimsan <aunpap.555@gmail.com>
2021-09-07 14:31:17 +0700

    ล่าสุด

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,138 @@
+//import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+//import java.io.FileOutputStream;
+//import java.net.Socket;
+//import java.nio.file.Files;
+//import java.util.ArrayList;
+//import javax.swing.*;
+//import javax.swing.JOptionPane;
+//import javax.swing.JPanel;
+//import javax.swing.JTable;
+//
+//public class Client {
+//    Socket clientSocket;
+//    DataInputStream din;
+//    DataOutputStream dout;
+//    String[][] fileName;
+//
+//    public static void main(String[] args) {
+//        new Client().Model();
+//
+//    }
+//
+//    public Client() {
+//        // connect to server
+//        int file;
+//
+//        try {
+//            clientSocket = new Socket("localhost", 8087);
+//            din = new DataInputStream(clientSocket.getInputStream());
+//            dout = new DataOutputStream(clientSocket.getOutputStream());
+//            
+//
+//            file = din.readInt();
+//            fileName = new String[file][4];
+//            for (int i = 0; i < file; i++) {
+//                String n = din.readUTF();
+//                fileName[i][0] = n; // ชื่อไฟล์
+//            }
+//            for (int i = 0; i < file; i++) {
+//                fileName[i][1] = din.readUTF(); // ชนิดข้อมูลไฟล์
+//            }
+//            for (int i = 0; i < file; i++) {
+//                String n = "" + din.readLong(); // ขนาดไฟล์
+//                fileName[i][2] = n;
+//            }
+//            
+//            
+//
+//
+//        } catch (Exception e) {
+//            JOptionPane.showMessageDialog(null, "Can't Connected. Please Try Again!", "ERROR CONNECTED",
+//                    JOptionPane.ERROR_MESSAGE);
+//
+//        }
+//    }
+//
+//    public void Model() {
+//        JFrame frameFile = new JFrame();
+//        frameFile.setTitle("Client");
+//        frameFile.setSize(500, 800);
+//        frameFile.setVisible(true);
+//
+//        String[] col = { "File", "File Type", "Size" ,"Download" };
+////        String[] col = { "File", "Download" };
+//        JPanel jpFile = new JPanel();
+//        JTable jtFile = new JTable(fileName, col);
+//        
+//        JScrollPane jspFile = new JScrollPane(jtFile);
+//        jpFile.setBounds(0, 0, 500, 400);
+//        jpFile.add(jspFile);
+//        frameFile.getContentPane().add(jpFile);
+//        frameFile.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//    }
+//}
+
+
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import javax.swing.*;
+import javax.swing.table.*;
 
 public class Client {
+    Socket clientSocket;
+    DataInputStream din;
+    DataOutputStream dout;
+    Object[][] fileName;
+    int file;
     public static void main(String[] args) {
-        System.out.println("Hello");
+        new Client().Model();
+
     }
+
+    public Client() {
+        // connect to server
+        
+
+        try {
+            clientSocket = new Socket("localhost", 8087);
+            din = new DataInputStream(clientSocket.getInputStream());
+            dout = new DataOutputStream(clientSocket.getOutputStream());
+
+            file = din.readInt();
+            fileName = new Object[file][4];
+            for (int i = 0; i < file; i++) {
+                fileName[i][0] = din.readUTF();
+            }
+            for (int i = 0; i < file; i++) {
+                fileName[i][1] = din.readUTF(); // ชนิดข้อมูลไฟล์
+            }
+            for (int i = 0; i < file; i++) {
+                String n = "" + din.readLong(); // ขนาดไฟล์
+                fileName[i][2] = n;
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Can't Connected. Please Try Again!", "ERROR CONNECTED",
+                    JOptionPane.ERROR_MESSAGE);
+
+        }
+    }
+
+    public void Model() {
+        DownloadPage downloadPage = new DownloadPage();
+        downloadPage.setVisible(true);
+
+    }
+
+
 }
+
+
+
---
+++ src/DownloadPage.java
@@ -0,0 +1,202 @@
+import java.awt.*;
+
+import java.awt.event.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import javax.swing.*;
+
+import javax.swing.table.TableModel;
+
+import javax.swing.table.DefaultTableModel;
+
+import javax.swing.table.*;
+
+class DownloadPage extends JFrame
+
+{
+    Client client = new Client();
+
+    private JPanel topPanel, topPanel1;
+
+    private JTable table;
+
+    private JScrollPane scrollPane, scrollPane1;
+
+    private String[] columnNames = new String[2];
+
+    private String[][] dataValues;
+
+    JButton button = new JButton();
+
+    public DownloadPage()
+
+    {
+        DefaultTableModel model = new DefaultTableModel();
+
+        setTitle("Downloader");
+
+        setSize(500, 800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        topPanel = new JPanel();
+
+        topPanel.setLayout(new BorderLayout());
+
+        getContentPane().add(topPanel);
+
+        columnNames = new String[] { "File", "File Type", "Size" ,"Download" };
+
+        dataValues = new String[client.file][4];
+        for (int i = 0; i < client.file; i++) {
+            dataValues[i][0] = client.fileName[i][0].toString();
+            dataValues[i][1] = client.fileName[i][1].toString();
+            dataValues[i][2] = client.fileName[i][2].toString();
+            dataValues[i][3] = client.fileName[i][0].toString();
+        }
+        model.setDataVector(dataValues,columnNames);
+        //TableModel model = new myTableModel("owntable");
+
+        table = new JTable(model);
+
+        //table.setModel(model);
+
+        table.getColumn("Download").setCellRenderer(new ButtonRenderer(dataValues));
+
+        table.getColumn("Download").setCellEditor(new ButtonEditor(new JCheckBox()));
+
+        scrollPane = new JScrollPane(table);
+
+        topPanel.add(scrollPane, BorderLayout.CENTER);
+
+        button.addActionListener(
+
+                new ActionListener()
+
+                {
+
+                    public void actionPerformed(ActionEvent event)
+
+                {
+
+                        JOptionPane.showMessageDialog(null, "Do you want to download "+button.getName()+"?");
+                        try {
+                            DataOutputStream dout = new DataOutputStream(client.clientSocket.getOutputStream());
+                            DataInputStream din = new DataInputStream(client.clientSocket.getInputStream());
+                            dout.writeUTF(button.getName());
+                            String filePath = "C:/Users/tubti/OneDrive - Silpakorn University/Documents/Thread/Client/" + button.getName();
+                            byte [] data = new byte[(int)din.readInt()];
+                            din.readFully(data,0,data.length);
+                            File fileDownload = new File(filePath);
+                            try {
+                                FileOutputStream fout = new FileOutputStream(fileDownload);
+                                fout.write(data);
+
+                            } catch (Exception e) {
+                                //TODO: handle exception
+                            }
+
+                        } catch (Exception e) {
+                            //TODO: handle exception
+                        }
+
+                    }
+
+                }
+
+        );
+
+    }
+
+    class ButtonRenderer extends JButton implements TableCellRenderer {
+
+        public ButtonRenderer(String[][] dataValues) {
+            
+            setOpaque(true);
+
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value,
+
+                boolean isSelected, boolean hasFocus, int row, int column) {
+
+            setText("Download");
+            setName(value.toString());
+
+            return this;
+
+        }
+
+    }
+
+    class ButtonEditor extends DefaultCellEditor {
+
+        private String label;
+
+        public ButtonEditor(JCheckBox checkBox) {
+
+            super(checkBox);
+
+        }
+
+        public Component getTableCellEditorComponent(JTable table, Object value,
+
+                boolean isSelected, int row, int column) {
+
+            label = "Download";
+
+            button.setText(label);
+            button.setName(value.toString());
+
+            return button;
+
+        }
+
+        public Object getCellEditorValue() {
+
+            return new String(label);
+
+        }
+
+    }
+
+    public class myTableModel extends DefaultTableModel
+
+    {
+
+        String dat;
+
+        JButton button = new JButton("");
+
+        myTableModel(String tname)
+
+        {
+
+            super(dataValues, columnNames);
+
+            dat = tname;
+
+        }
+
+        public boolean isCellEditable(int row, int cols)
+
+        {
+
+            if (dat == "owntable")
+
+            {
+
+                if (cols == 0) {
+                    return false;
+                }
+
+            }
+
+            return true;
+
+        }
+
+    }
+}
